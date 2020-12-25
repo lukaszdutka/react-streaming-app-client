9690685611d01a67d33810a212dbd5dcdf8a85e8
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-25 21:32:14 +0100

    render something based on being signed on;

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = {isSignedIn: null}
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
-        clientId: '',
+        clientId: '567276739995-p3upptnkfe8f80mqpb2sl505fgnlst1h.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({isSignedIn: this.auth.isSignedIn.get()})
       });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return 'idk bro'
+    } else if (this.state.isSignedIn) {
+      return 'I am signed in!'
+    } else {
+      return 'I am NOT signed in!'
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return (
+      <div>
+        {this.renderAuthButton()}
+      </div>
+    );
   }
 }
 
