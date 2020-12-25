3caa2e932ef8a06a3d8ad053e07fa501ee28cb48
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-25 21:49:12 +0100

    sign in and sign out with buttons;

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,25 +11,46 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({isSignedIn: this.auth.isSignedIn.get()})
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()})
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return 'idk bro'
+      return null;
     } else if (this.state.isSignedIn) {
-      return 'I am signed in!'
+      return (
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      );
     } else {
-      return 'I am NOT signed in!'
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
+      );
     }
   }
 
   render() {
     return (
-      <div>
-        {this.renderAuthButton()}
-      </div>
+      this.renderAuthButton()
     );
   }
 }
