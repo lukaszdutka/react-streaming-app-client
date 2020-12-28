fa60314aae38d11377b68e7aea69e2d46564b6fb
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-28 14:48:37 +0100

    wire up redux-thunk middleware; create async action for creating stream; connect both redux-form and redux to component; create simple axios api

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Field, reduxForm} from 'redux-form';
+import {createStream} from "../../actions";
+import {connect} from 'react-redux';
 
 
 class StreamCreate extends React.Component {
@@ -25,8 +27,8 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -51,7 +53,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
