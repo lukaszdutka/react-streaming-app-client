24244552fda834e1565e68c86e6a383d652de941
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-28 18:09:04 +0100

    create and wireUp streamReducer;

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS} from '../actions/types'
+import _ from 'lodash';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      const keys = _.mapKeys(action.payload, 'id');
+      return {...state, ...keys}
+    case FETCH_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case CREATE_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case EDIT_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case DELETE_STREAM:
+      return _.omit(state, action.payload)
+    default:
+      return state;
+  }
+};
