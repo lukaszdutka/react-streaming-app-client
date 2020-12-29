01f742390dac7eee76cdc8e0596926b181f1f873
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-29 11:31:00 +0100

    add spacing;

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,15 +26,19 @@ export const fetchStreams = () => async dispatch => {
 
   dispatch({type: FETCH_STREAMS, payload: response.data});
 }
+
 export const fetchStream = id => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
 
   dispatch({type: FETCH_STREAM, payload: response.data});
 }
+
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
+
   dispatch({type: DELETE_STREAM, payload: id});
 }
+
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
