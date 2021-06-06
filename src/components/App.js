ad9c7cad21171dd85d55b62d216705372919a1d1
lukaszdutka <lukaszdutka2@gmail.com>
2021-06-06 11:56:01 +0200

    create StreamShow component; add React Switch to app.js;

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Router} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
@@ -9,20 +9,22 @@ import Header from "./Header";
 import history from '../history';
 
 const App = () => {
-  return (
-    <div className="ui container">
-      <Router history={history}>
-        <div>
-          <Header/>
-          <Route path="/" exact component={StreamList}/>
-          <Route path="/streams/new" exact component={StreamCreate}/>
-          <Route path="/streams/edit/:id" exact component={StreamEdit}/>
-          <Route path="/streams/delete/:id" exact component={StreamDelete}/>
-          <Route path="/streams/show" exact component={StreamShow}/>
+    return (
+        <div className="ui container">
+            <Router history={history}>
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route path="/" exact component={StreamList}/>
+                        <Route path="/streams/new" exact component={StreamCreate}/>
+                        <Route path="/streams/edit/:id" exact component={StreamEdit}/>
+                        <Route path="/streams/delete/:id" exact component={StreamDelete}/>
+                        <Route path="/streams/:id" exact component={StreamShow}/>
+                    </Switch>
+                </div>
+            </Router>
         </div>
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
